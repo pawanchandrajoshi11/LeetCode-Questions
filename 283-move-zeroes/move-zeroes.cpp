4b9bacf2b05ee76bef301e936c504e28e6ad8d62
Pawan Chandra Joshi <73007506+pawanchandrajoshi11@users.noreply.github.com>
2023-11-20 13:46:29 +0530

    Time: 18 ms (53.71%) | Memory: 19.7 MB (36.33%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        // Optimal Approach - O(N)
+        // first find the first zero and mark its index
+        int j=-1;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] == 0){
+                j=i;
+                break;
+            }
+        }
+
+        // returning if the array doesn't have any zeros
+        if(j == -1){
+            return;
+        }
+
+        // moving zeros algo
+        for(int i=j+1; i<nums.size(); i++){
+            if(nums[i] != 0){
+                swap(nums[j], nums[i]);
+                j++;
+            }
+        }
+    }
+};
