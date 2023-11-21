f0585ee5f8ccbafa1def66a9aa8f06e04760c817
Pawan Chandra Joshi <73007506+pawanchandrajoshi11@users.noreply.github.com>
2023-11-21 23:31:37 +0530

    Time: 3 ms (92.01%) | Memory: 13.9 MB (59.63%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,29 +1,79 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        // BETTER SOLN
-        int first = -1, last = -1;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] == target){
-                if(first == -1){
-                    first = i;
-                }
-                last = i;
+    int firstOccurance(vector<int> &nums,int n, int target){
+        int low=0, high=n-1;
+        int first = -1;
+        while(low <= high){
+            int mid = low+(high-low)/2;
+
+            if(nums[mid] == target){
+                first = mid;
+                high = mid - 1; // as it is already present, in order to find the first occurance we will look into the left half.
+            }
+            else if(nums[mid] < target){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
             }
         }
+        return first;
+    }
 
-        vector<int> ans;
+    int lastOccurance(vector<int> &nums, int n, int target){
+        int low=0, high=n-1;
+        int last = -1;
+        while(low <= high){
+            int mid = low+(high-low)/2;
 
-        if(first == -1){
-            ans.push_back(-1);
-            ans.push_back(-1);
-
-            return ans;
+            if(nums[mid] == target){
+                last = mid;
+                low = mid + 1; // as it is already present, in order to find the last occurance we will look into the right half.
+            }
+            else if(nums[mid] < target){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
         }
-        
-        ans.push_back(first);
-        ans.push_back(last);
+        return last;
+    }
 
-        return ans;
+    vector<int> searchRange(vector<int>& nums, int target) {
+        // OPTIMAL APPROACH USING LOWER AND UPPER BOUND
+        int n=nums.size();
+        int first = firstOccurance(nums, n, target);
+        if(first == -1){
+            return {-1,-1};
+        }
+
+        int last = lastOccurance(nums, n, target);
+
+        return {first, last};
+
+
+
+        
+        // // BETTER SOLN
+        // int first = -1, last = -1;
+        // for(int i=0; i<nums.size(); i++){
+        //     if(nums[i] == target){
+        //         if(first == -1){
+        //             first = i;
+        //         }
+        //         last = i;
+        //     }
+        // }
+
+        // vector<int> ans;
+
+        // if(first == -1){
+        //     ans.push_back(-1);
+        //     ans.push_back(-1);
+        //     return ans;
+        // }
+        
+        // ans.push_back(first);
+        // ans.push_back(last);
+        // return ans;
     }
 };
