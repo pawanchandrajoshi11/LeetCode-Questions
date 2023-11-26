47cf27f5168541706c396a963bb51a3c8a540ff6
Pawan Chandra Joshi <pawanpcj600@gmail.com>
2023-11-26 23:53:27 +0530

    Time: 39 ms (48.16%) | Memory: 19.4 MB (36.79%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -2,42 +2,26 @@ class Solution {
 public:
     double calcTime(vector<int> &piles, int hourlyEat){
         double totalH = 0;
-        for(int i=0; i<piles.size(); i++){
+        for(int i=0; i< piles.size(); i++){
             totalH += ceil((double)piles[i]/(double)hourlyEat);
         }
         return totalH;
     }
-
     int minEatingSpeed(vector<int>& piles, int h) {
-        //OPTIMISED APPROACH - O(logN + N) using binary search
-        int n=piles.size();
-        sort(piles.begin(), piles.end());
-        int maxEle = piles[n-1];
-        int low=1, high=maxEle;
+        int maxi = INT_MIN;
+        for(int i=0; i<piles.size(); i++){
+            maxi = max(maxi, piles[i]);
+        }
+
+        int low=1, high=maxi;
         while(low <= high){
             int mid = low+(high-low)/2;
-
-            double reqTime = calcTime(piles, mid);
-            if(reqTime <= h){
+            if(calcTime(piles, mid) <= h){
                 high = mid-1;
             }else{
                 low = mid+1;
             }
         }
         return low;
-
-
-        // BRUTE APPROACH - O(max(piles) * n) - TIME LIMIT EXCEEDED
-        // sort(piles.begin(), piles.end());
-        // int n = piles.size();
-        // int max = piles[n-1];
-        // for(int i=1; i<= max; i++){
-        //     int reqTime = calcTime(piles, i);
-
-        //     if(reqTime <= h){
-        //         return i;
-        //     }
-        // }
-        // return -1;
     }
 };
