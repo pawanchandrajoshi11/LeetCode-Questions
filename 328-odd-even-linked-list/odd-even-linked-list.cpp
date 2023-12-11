97c2465c7aea0279be3ac9391a56d66a7a0905e3
Pawan Chandra Joshi <pawanpcj600@gmail.com>
2023-12-12 02:13:35 +0530

    Time: 11 ms (28.42%) | Memory: 10.7 MB (71.99%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -11,35 +11,63 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
+
+        //OPTIMISED - WITHOUT USING EXTRA SPACE
         if(head == NULL || head->next == NULL){
             return head;
         }
-        ListNode *temp = head;
-        vector<int> arr;
-        while(temp != NULL && temp->next != NULL){
-            arr.push_back(temp->val);
-            temp = temp->next->next;
-        }
-        if(temp){
-            arr.push_back(temp->val);
+        ListNode *odd = head;
+        ListNode *even = head->next;
+        ListNode *evenHead = head->next;
+
+        while(even != NULL && even->next != NULL){
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+
+            odd = odd->next;
+            even = even->next;
         }
 
-        temp = head->next;
-        while(temp != NULL && temp->next != NULL){
-            arr.push_back(temp->val);
-            temp = temp->next->next;
-        }
-        if(temp){
-            arr.push_back(temp->val);
-        }
-
-        int i=0;
-        temp = head;
-        while(temp != NULL){
-            temp->val = arr[i];
-            i++;
-            temp = temp->next;
-        }        
+        odd->next = evenHead;
         return head;
+
+
+
+
+
+
+
+
+        // BRUTE: TC: O(2N), SC: O(N)
+        // if(head == NULL || head->next == NULL){
+        //     return head;
+        // }
+        // ListNode *temp = head;
+        // vector<int> arr;
+        // while(temp != NULL && temp->next != NULL){
+        //     arr.push_back(temp->val);
+        //     temp = temp->next->next;
+        // }
+        // if(temp){
+        //     arr.push_back(temp->val);
+        // }
+
+        // temp = head->next;
+        // while(temp != NULL && temp->next != NULL){
+        //     arr.push_back(temp->val);
+        //     temp = temp->next->next;
+        // }
+        // if(temp){
+        //     arr.push_back(temp->val);
+        // }
+
+        // int i=0;
+        // temp = head;
+        // while(temp != NULL){
+        //     temp->val = arr[i];
+        //     i++;
+        //     temp = temp->next;
+        // }        
+        // return head;
     }
 };
