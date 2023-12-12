623e090558e08663cce849fe25fcefa0a48c75d2
Pawan Chandra Joshi <pawanpcj600@gmail.com>
2023-12-12 21:40:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (38.51%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int maxi = INT_MIN;
-        for(int i=0; i<nums.size()-1; i++){
-            for(int j=i+1; j<nums.size(); j++){
-                int a = nums[i]-1;
-                int b = nums[j]-1;
-                int temp = a * b;
-                maxi = max(maxi, temp);
+
+        //OPTIMAL: FIND THE BIGGEST AND SECOND BIGGEST NUMBER
+        int largest = INT_MIN;
+        int secondLargest = INT_MIN;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] > largest){
+                secondLargest = largest;
+                largest = nums[i];
+            }else if(nums[i] > secondLargest){
+                secondLargest = nums[i];
             }
         }
-        return maxi;
+        int product = (largest-1) * (secondLargest-1);
+        return product;
+
+        // int maxi = INT_MIN;
+        // for(int i=0; i<nums.size()-1; i++){
+        //     for(int j=i+1; j<nums.size(); j++){
+        //         int a = nums[i]-1;
+        //         int b = nums[j]-1;
+        //         int temp = a * b;
+        //         maxi = max(maxi, temp);
+        //     }
+        // }
+        // return maxi;
     }
 };
