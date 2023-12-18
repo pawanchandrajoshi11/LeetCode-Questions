aa5ac4c52820a47427c4814a7b6f9b1cda4f816a
Pawan Chandra Joshi <pawanpcj600@gmail.com>
2023-12-18 15:13:35 +0530

    Time: 21 ms (70.62%) | Memory: 20.8 MB (28.86%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.cpp
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.cpp
@@ -2,17 +2,45 @@ class Solution {
 public:
     int maxProductDifference(vector<int>& nums) {
 
-        // TC: O(N logN)
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        int a = nums[n-1];
-        int b = nums[n-2];
-        int c = nums[0];
-        int d = nums[1];
+        // ans = (largest * secondLargest) - (smallest * secondSmallest)
+        // TC: O(N)
+        int a = INT_MIN;    // largest
+        int b = INT_MIN;    // second largest
+        int c = INT_MAX;    // smallest
+        int d = INT_MAX;    // second smallest
+        for(int i=0; i<nums.size(); i++){
 
-        long long first = a * b;
-        long long second = c * d;
+            // finding largest and second largest
+            if(nums[i] > a){
+                b = a;
+                a = nums[i];
+            }else if(nums[i] > b){
+                b = nums[i];
+            }
 
-        return first - second;
+            // finding smallest and second smallest
+            if(nums[i] < c){
+                d = c;
+                c = nums[i];
+            }else if(nums[i] < d){
+                d = nums[i];
+            }
+        }
+        return (a*b)-(c*d);
+
+
+
+        // APPROACH 2 -> TC: O(N logN)
+        // sort(nums.begin(), nums.end());
+        // int n = nums.size();
+        // int a = nums[n-1];
+        // int b = nums[n-2];
+        // int c = nums[0];
+        // int d = nums[1];
+
+        // long long first = a * b;
+        // long long second = c * d;
+
+        // return first - second;
     }
 };
